dc6f537d9f72d3abb49d61a6eda3c452886ebdd2
Scott Sher <scottslinux@gmail.comclear>
2025-03-01 18:06:20 -0700

    working so far

--- myProject.cpp
+++ myProject.cpp
@@ -5,6 +5,9 @@
 
 int main() {
 
+    int NUMBODIES=5;
+    b2BodyId bodyIDs[NUMBODIES];
+
     float posx=0;
     float posy=0;
 
@@ -29,48 +32,57 @@ int main() {
     b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
 
 // ⁡⁣⁢⁣⁡⁣⁢⁣𝗖𝗿𝗲𝗮𝘁𝗲 𝗮 𝗱𝘆𝗻𝗮𝗺𝗶𝗰 𝗯𝗼𝗱𝘆.⁡
+
+for (int i=0;i<NUMBODIES;i++)
+{
     b2BodyDef bodyDef = b2DefaultBodyDef();
     bodyDef.type = b2_dynamicBody;
-    bodyDef.angularVelocity=20.0f;
-    bodyDef.position = (b2Vec2){0.0f, 20.0f};
-    b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
+    bodyDef.angularVelocity=10.0f;
+    bodyDef.position = (b2Vec2){0.0f, 25.0f + i * 2.0f};
+    bodyIDs[i] = b2CreateBody(worldId, &bodyDef);
 
-    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
+    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);  // 1 meter x 1 meter
     b2ShapeDef shapeDef = b2DefaultShapeDef();
     shapeDef.density = 1.0f;
-    shapeDef.friction = 0.0f;
+    shapeDef.friction = 0.5f;
     shapeDef.restitution = 0.4f;
     
-    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
-
+    b2CreatePolygonShape(bodyIDs[i], &shapeDef, &dynamicBox);
+}
  // ⁡⁣⁢⁣𝗦𝘁𝗮𝗿𝘁 𝘁𝗵𝗲 𝗦𝗶𝗺𝘂𝗹𝗮𝘁𝗶𝗼𝗻⁡
 
     float timeStep = 1.0f / 60.0f;
     int substepCount = 4;
     
 while(!WindowShouldClose()) {
+
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    for(int i=0;i<NUMBODIES;i++)
+    {
     // Step physics
     b2World_Step(worldId, timeStep, substepCount);
 
     // Get physics body position
-    b2Vec2 position = b2Body_GetPosition(bodyId);
-    b2Rot rotation = b2Body_GetRotation(bodyId);
+    b2Vec2 position = b2Body_GetPosition(bodyIDs[i]);
+    b2Rot rotation = b2Body_GetRotation(bodyIDs[i]);
 
-    b2Vec2 positionGround =b2Body_GetPosition(groundId);
+    
 
 
     // Convert meters to pixels
     float screenX = position.x * 50 + 500;
-    float screenY = 1500 - (position.y * 50);
+    float screenY = 1500 - (position.y * 50);  //50 pixels / meter
 
     // Drawing
-    BeginDrawing();
-    ClearBackground(WHITE);
+    
     
     Rectangle myrect = {screenX, screenY, 50, 50};
-    DrawRectanglePro(myrect, {25, 25}, b2Rot_GetAngle(rotation) * RAD2DEG, BLACK);
-
-    DrawRectangle(0,1500-(positionGround.y*50),1000,1500-(positionGround.y*50+20),BLACK);
+    DrawRectanglePro(myrect, {25, 25}, b2Rot_GetAngle(rotation) * RAD2DEG, BLACK);  //falling cube
+    }
+    b2Vec2 positionGround =b2Body_GetPosition(groundId);
+    DrawRectangle(0,1500-(positionGround.y*50),1000,1500-(positionGround.y*50+20),BLACK);  //ground delineation
 
 
     DrawRectangle(0,1475,1000,5,BLACK);
@@ -80,7 +92,10 @@ while(!WindowShouldClose()) {
     EndDrawing();
 }
 
+
     b2DestroyWorld(worldId);
     std::cout << "Box2D is working!" << std::endl;
+
     return 0;
+
 }
